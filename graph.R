2e2e440b870e70829ea379611dc974f6421fc9c0
Colin Rundel <rundel@gmail.com>
2016-02-05 10:35:48 -0500

    Function stubs

---
+++ graph.R
@@ -0,0 +1,28 @@
+is_valid = function(g)
+{
+    return(FALSE)
+}
+
+
+is_undirected = function(g)
+{
+    return(FALSE)
+}
+
+
+is_isomorphic(g1, g2)
+{
+    return(FALSE)
+}
+
+    
+is_connected(g, v1, v2)
+{
+    return(character())
+}
+
+
+shortest_path(g, v1, v2)
+{
+    return(character())
+}
