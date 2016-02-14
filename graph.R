b92efaf9fe0c01c9a7742b517e0c5455bc368667
Jenny Li <yl266@duke.edu>
2016-02-14 18:40:31 -0500

    first four, passed all test cases

--- graph.R
+++ graph.R
@@ -71,62 +71,69 @@ is_valid = function(g) {
   return (TRUE)
 }
 
-
 is_undirected = function(g){
   if (is_valid(g)==FALSE){
-    return(FALSE)
+    stop('error')
   }
   j=adj_matrix(g)
-  q=t(adj_matrix(g))
-  if (j!=q){
-    return (FALSE)
-  }else{return (TRUE)}
+  for (i in 1:length(g)){
+    for (k in i:length(g)){
+      if (j[i,k]!=j[k,i]){
+        return (FALSE)
+      }
+    }
   }
+  return(TRUE)
+}
 
 is_isomorphic = function(g1, g2){ 
   if(is_valid(g1)==FALSE & is_valid(g2)==FALSE){
+    stop ("error")
+  }
+  if (length(g1)!=length(g2)){
+    return(FALSE)
+  }
+  if(length(setdiff(names(g1), names(g2)))!=0 && length(setdiff(names(g2), names(g1)))!=0){
     return (FALSE)
   }
-  else if(sorted(names(g1))==sorted(names(g2))){
-    adj1<-adj_matrix(g1)
-    adj2<-adj_matrix(g2)
-    list1<-lapply(seq_len(nrow(adj1)), function(i) sort(adj1[i,]))
-    list2<-lapply(seq_len(nrow(adj2)), function(i) sort(adj2[i,]))
-    
-    return (prod(list2 %in% list1)==1)
-  }else{
-    return (FALSE)
+  a=adj_matrix(g1)
+  b=adj_matrix(g2)
+  for(i in names(g1)){
+    for(j in names(g1)){
+      if (a[i,j]!=b[i,j]){
+        return (FALSE)
+      }
+    }
   }
+  return (TRUE)
 }
-
 
 is_connected=function(g, v1, v2){
   v1exist=FALSE
   v2exist=FALSE
   indexv1=0
   indexv2=0
-  
-  #check if is character not number and not array 
-  if(is.character(v1)==FALSE | length(v1)>1){
-    return(FALSE)
+  if (is_valid(g)==FALSE){
+    stop("error")
   }
-  
+  #check if is vertex not number and not array 
+  if(is.character(v1)==FALSE | length(v1)>1){
+    stop("error")
+  }
   if(is.character(v2)==FALSE | length(v2)>1){
-    return(FALSE)
+    stop("error")
   }
   #check if v1 and v2 exissts in the graph
   for(i in 1:length(g)){
     if(names(g[i]) == v1){
       v1exist=TRUE
     }
-    
     if(names(g[i]) == v2){
       v2exist=TRUE
     }
   }
-  
   if(v1exist == FALSE|v2exist == FALSE){
-    return(FALSE)
+    stop("error")
   }
   #find index of the v1 and v2 on the graph/adjmatrix
   for(i in 1:length(g)){
@@ -142,83 +149,33 @@ is_connected=function(g, v1, v2){
     if(length(g[[indexv1]]$edges) == 0){ 
       return(FALSE)
     } 
-    else {
-      for (i in 1:length(g[[indexv1]]$edges)){
-        if(g[[indexv1]]$edges[i] == indexv2 ){
-          return(TRUE)
-        } 
-        else {
-          return(FALSE)
+    else if (indexv1 %in% g[[indexv1]]$edges){
+      return (TRUE)
+    }
+    else{return (FALSE)}
+  }
+  else if(v1 != v2){
+    queue=integer()
+    visited=integer()
+    queue=c(queue, indexv1)
+    while(length(queue)>0){
+      pop=queue[1]
+      queue=queue[-1]
+      if(!pop%in% visited){
+        visited=c(visited, pop)
+        for (values in g[[pop]]$edges){
+          if(!values%in%visited){
+            queue=c(queue, values)
+            #print(queue)
+          }
         }
       }
     }
-  }
-  
-  
-  visited=list()
-  visited=c(visited, indexv1)
-  k=adj_matrix(g)
-  n=length(g)
-  while(n>0){
-    for (j in 1:length(visited)){
-      for ( i in 1:length(g)){
-        if (k[visited[[j]], i]>0){
-          visited=c(visited, i)
-        }
-      }
+    #print(visited)
+    for( i in 1:length(visited)){
+      if (visited[i]==indexv2)
+        return(TRUE)
     }
-    n=n-1
+    return(FALSE)
   }
-  
-  for( i in 1:length(visited)){
-    if (visited[[i]]==indexv2)
-      return(TRUE)
-  }
-  return(FALSE)
-  
-  
-  
 }
-
-shortest_path = function(g, v1, v2)
-{
-  return(character())
-}
-
-adj_matrix <- function(g)
-{
-  stopifnot(is_valid(g))
-  
-  m = matrix(0,nrow=length(g), ncol=length(g))
-  rownames(m) = names(g)
-  colnames(m) = names(g)
-  for (j in seq_along(g))
-  {
-    node = g[[j]]
-    
-    
-    for(i in seq_along(node$edges))
-    {
-      m[j, node$edges[i]] = node$weights[i]
-    }
-  }
-  return(m)
-}
-
-
-
-graph1 = list(list(edges   = c(2L),
-                       weights = c(1 )),
-              A=list(edges   = c(3L),
-                       weights = c(1 )),
-              list(edges   = c(5L),
-                       weights = c(1 )),
-              list(edges   = c(2L),
-                       weights = c(1 )),
-              list(edges   = c(4L,6L),
-                       weights = c(1,1  )),
-              list(edges   = c(),
-                       weights = c())
-)
-
-str(graph1)
