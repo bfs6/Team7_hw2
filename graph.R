5344743f74a463d23d78abe9c5a81ac9b5322e75
Basil Seif <bfs6@duke.edu>
2016-02-14 22:40:40 +0000

    Commit working is_valid and is_undirected

--- graph.R
+++ graph.R
@@ -1,89 +1,92 @@
 is_valid = function(g) {
   #Remark: in a valid graph, some vertices have empty vectors for edges and vertices if they don't point to anywhere
-  #Check that g is a list of lists
-  v = c()
-  v=c(v,is.list(g))
-  
-  for (i in g){
-    v=c(v,is.list(i))
+  #1. Check that g is a list of lists
+  if(length(g)<1){
+    print(1) 
+    return (FALSE)
+  }else if(!is.list(g)){
+    print(1.1) 
+    return(FALSE)
+  }else if(prod(sapply(g,is.list))!=1){
+    print(1.2) 
+    return (FALSE)
   }
-  
-  #Check that the list names are unique
-  v=c(v,nlevels(factor(names(g))) == length(factor(names(g))))
-  
-  #Check that each secondary list contains only edges and weights vectors that are of the appropriate type.
+  #2. Missing labels
+  if(is.null(names(g))){
+    print(2) 
+    return(FALSE)
+  }else if(TRUE %in% (sapply(names(g),function(x){x==""}))){
+    print(2.1) 
+    return (FALSE)
+  }
+  #missing labels also caught all the testing cases for invalid structure
+  #3. invalid structure
+  if(mean(sapply(g,length))!=2){
+    print(3) 
+    return (FALSE)
+  }
+  #4. duplicate labels
+  if(length(names(g))!=length(unique(names(g)))){
+    print(4)
+    return(FALSE)
+  }
+  #5. invalid vertex reference
+  if(!FALSE %in% sapply(g,function(x){length(x$edges)>0 & length(x$weights)>0}) & ! NA %in% unlist(g)){
+    if(max(sapply(g,function(x){max(x$edges)})) > length(g)){
+      print(5)
+      return (FALSE)
+    }
+  }
+  #6. duplicate vertex
+  if(length(unique(names(g))) != length(g)){
+    print(6) 
+    return (FALSE)
+  }
+  #7. edge type:
+  for(i in g){
+    if (FALSE %in% (sapply(i$edges,is.integer)) | NA %in% i$edges){
+      print("bad edge")
+      return (FALSE)
+    }
+  }
+  #the test cases returns as false but I don't think they made it this far
+  #8. Weight type and value
+  for(i in g){
+    if (FALSE %in% (sapply(i$weights,is.double)) | NA %in% i$weights | FALSE %in% (sapply(i$weights,function(x){x>0}))){
+      print("bad weight")
+      return (FALSE)
+    }
+  }
+  #9. duplicate edges
+  if(TRUE %in% sapply(g,function(x){duplicated(x$edges)})){
+    print(9) 
+    return (FALSE)
+  }
+  #10 edge and weight lenth mismatch
+  if(FALSE %in% sapply(g,function(x){length(x$edges) == length(x$weights)})){
+    print(10) 
+    return (FALSE)
+  }
+  #if all cases passed then the graph is valid
+  return (TRUE)
+}
+
+
+
+is_undirected = function(g){
+  if (is_valid(g)==FALSE){
+    stop('error')
+  }
+  j=adj_matrix(g)
   for (i in 1:length(g)){
-    v=c(v,is.integer(g[[i]][[1]])|is.null(g[[i]][[1]]))  
-    if(is.null(g[[i]][[2]])){v=c(v,TRUE)}
-    else{
-      for (x in g[[i]][[2]]){
-        v=c(v,is.double(x))
+    for (k in i:length(g)){
+      if (j[i,k]!=j[k,i]){
+        return (FALSE)
       }
     }
   }
-  
-  # Check that there are not any edges to non-existent vertices
-  for (i in 1:length(g)){
-    v=c(v,prod(g[[i]][[1]]<=length(g)) == 1)
-  }
-  
-  # Check that all weights are greater than 0.
-  for (i in 1:length(g)){
-    v=c(v,prod(g[[i]][[2]]>0) == 1)
-  }
-  
-  print(v)
-  return(prod(v)==1)
+  return(TRUE)
 }
-is_valid(graph1)
-is_valid(graph2)
-
-
-typeof(graph1)
-
-is_undirected = function(g)
-{
-    return(FALSE)
-}
-
-
-is_isomorphic = function(g1, g2)
-{
-    return(FALSE)
-}
-
-    
-is_connected = function(g, v1, v2)
-{
-    return(character())
-}
-
-
-shortest_path = function(g, v1, v2)
-{
-    return(character())
-}
-
-adj_matrix <- function(g)
-{
-  stopifnot(is_valid(g))
-  
-  m = matrix(0,nrow=length(g), ncol=length(g))
-  rownames(m) = names(g)
-  colnames(m) = names(g)
-  for (j in seq_along(g))
-  {
-    node = g[[j]]
-    
-  
-    for(i in seq_along(node$edges))
-    {
-      m[j, node$edges[i]] = node$weights[i]
-    }
-  }
-  return(m)
-}
-
 graph1 = list(A = list(edges   = c(2L),
                        weights = c(1 )),
               B = list(edges   = c(3L),
